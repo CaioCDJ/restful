126ad9d14bbbd46cbb013b95f7ae1bcc5e854030
CaioCDJ <caiodjesus1@hotmail.com>
2022-05-09 15:52:12 -0300

    Data persistence with NeDB

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,12 @@
 
+// like mongo
+const { log } = require('console');
+let NeDB = require('nedb');
+let db = new NeDB({
+    filename: 'users.db',
+    autoload: true
+});
+
 module.exports = (app) => {
     app.get('/users',(req,res)=>{
         console.log('url: ', req.url);
@@ -16,7 +24,16 @@ module.exports = (app) => {
         })
     });
     app.post('/users',(req,res)=>{
-     
-        res.json(req.body);
+             
+        db.insert(req.body,(err,user)=>{
+            if(err){
+                console.log(err);
+                res.status(400).json({
+                    error: err
+                });
+            } else {
+                res.status(200).json(user);
+            }
+        });
     });
 };
